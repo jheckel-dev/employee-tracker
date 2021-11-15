1d7d2376165bd0f4969028a1ddcd3203c1b12c76
jheckel <jalen.heckel@icloud.com>
2021-11-15 12:13:18 -0800

    got database created and functioning. Will finish assignment tonight afterwork

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -1,6 +1,6 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
-USE employee_db
+USE employee_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT,
--- server.js
+++ server.js
@@ -1,3 +1,20 @@
 const inquirer = require("inquirer");
-var mysql = require("mysql");
+var mysql = require("mysql2");
 var cTable = require("console.table");
+
+require('dotenv').config()
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "spacejam",
+    database: 'employee_db'
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Welcome to the employee tracker!");
+    console.log("--------------------------------")
+    // init();
+  });
