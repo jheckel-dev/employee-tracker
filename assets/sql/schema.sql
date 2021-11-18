0030ddb492831de6586ba37f5655d09438852033
jheckel <jalen.heckel@icloud.com>
2021-11-17 19:27:49 -0800

    app is functioning, demo gif added to readme

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -1,29 +1,80 @@
+
+
 DROP DATABASE IF EXISTS employee_db;
+
 CREATE DATABASE employee_db;
+
 USE employee_db;
 
+
 CREATE TABLE department (
-    id INT AUTO_INCREMENT,
-    name VARCHAR(30).
-    PRIMARY KEY(id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+ 
 );
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT NOT NULL,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT,
-    PRIMARY KEY(id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    PRIMARY key(employee_id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(employee_id)
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
+
 );
+
+
+INSERT INTO department (name)
+VALUE ("Sales");
+INSERT INTO department (name)
+VALUE ("Engineering");
+INSERT INTO department (name)
+VALUE ("Finance");
+INSERT INTO department (name)
+VALUE ("Legal");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lead Engineer", 150000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Legal Team Lead", 250000, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Accountant", 125000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Sales Lead", 100000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Salesperson", 80000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Software Engineer", 120000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Lawyer", 190000, 4);
+
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Jerry", "Seinfeld", null, 1);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Elaine", "Benes", null, 2);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("George","Costanza",null,3);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Newmain", "The Mailman", 1, 4);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Cosmo", "Kramer", 4, 5);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Allen", "Alda", 1, 6);
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUE ("Clark", "Gable", 2, 7);
+
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
